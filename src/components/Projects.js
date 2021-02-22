3b8a6ab8a2975b2916d9df3ec85d44fadc27d466
aes89 <adrienne.e.smith@outlook.com>
2021-02-22 16:59:27 +1100

    Fix: Add Project to List

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,10 @@ class Projects extends Component {
         <h2>Projects:</h2>
         <ul>
           <li>
+            <a href="https://fridgemate.netlify.app/ ">FridgeMate</a>: Final
+            MERN stack project, completed with one other student.
+          </li>
+          <li>
             <a href="https://blog-hackathon.herokuapp.com/">Blog</a>: hackathon
             project with one other student, using react.
           </li>
@@ -16,7 +20,7 @@ class Projects extends Component {
           </li>
           <li>
             <a href="https://bulb-hackathon.netlify.app/">Plantr.</a>:
-            marketplace app built Ruby on Rails, without styling.
+            marketplace app built Ruby on Rails, styling was not required.
           </li>
           <li>
             <a href="https://aes89.github.io/">Portfolio</a>: portfolio built
