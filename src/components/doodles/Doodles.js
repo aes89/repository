441d9c73262e92cbd901dffe23c40966ee07c497
aes89 <adrienne.e.smith@outlook.com>
2021-02-22 23:41:40 +1100

    Fix: Removed Unused Vars

--- src/components/doodles/Doodles.js
+++ src/components/doodles/Doodles.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import { Route, NavLink, HashRouter } from "react-router-dom";
+import React from "react";
 import "../../index.css";
 import Cartman from "./cartman";
 import Koala from "./koala";
